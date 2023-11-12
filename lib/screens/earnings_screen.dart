12e9bc48f482de7fba32f6e77189ef724c5650cc
Baltazar Christian (Tazar Chriss) <120463084+Baltazar-Christian@users.noreply.github.com>
2023-11-12 16:44:25 +0300

    fixed

---
+++ lib/screens/earnings_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class EarningsScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Earnings'),
+      ),
+      body: Center(
+        child: Text('Earnings Screen Content'),
+      ),
+    );
+  }
+}
