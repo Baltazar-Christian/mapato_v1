175bcace5b431b78c84d14613cb846798a88ec15
Baltazar Christian (Tazar Chriss) <120463084+Baltazar-Christian@users.noreply.github.com>
2023-11-12 16:44:52 +0300

    modified

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -1,24 +1,75 @@
 import 'package:flutter/material.dart';
-import 'home_screen.dart'; // Import the HomeScreen
 
-class MainScreen extends StatelessWidget {
+import 'home_screen.dart';
+import 'earnings_screen.dart';
+import 'savings_screen.dart';
+import 'expenses_screen.dart';
+import 'debts_screen.dart';
+
+class MainScreen extends StatefulWidget {
+  @override
+  _MainScreenState createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  int _currentIndex = 0;
+
+  final List<Widget> _screens = [
+    HomeScreen(),
+    EarningsScreen(),
+    SavingsScreen(),
+    ExpensesScreen(),
+    DebtsScreen(),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Main Screen'),
+        title: Text('Your App Name'),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.logout),
+            onPressed: () {
+              // Implement logout logic here
+              // For example, navigate back to the RegistrationScreen
+              // Navigator.of(context).popUntil((route) => route.isFirst);
+            },
+          ),
+        ],
       ),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            // Navigate to HomeScreen instead of CustomLayoutScreen
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => HomeScreen()),
-            );
-          },
-          child: Text('Go to Home Screen'),
-        ),
+      body: _screens[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.monetization_on),
+            label: 'Earnings',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.account_balance_wallet),
+            label: 'Savings',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.money_off),
+            label: 'Expenses',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.payment),
+            label: 'Debts',
+          ),
+        ],
+        selectedItemColor: Colors.blue,
+        unselectedItemColor: Colors.grey, // Set unselected item color
+        currentIndex: _currentIndex,
+        onTap: (index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
       ),
     );
   }
